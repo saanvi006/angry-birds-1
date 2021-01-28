75c3171a23adc899e2b9782513e31d5b0ea840e9
saanvi006 <74246059+saanvi006@users.noreply.github.com>
2021-01-28 17:38:52 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,36 @@
+class slingshot{
+constructor (bodyA, pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness:0.05,
+            length:7
+        }
+        this.pointB = pointB
+        this.slingshot = Constraint.create(options)
+        World.add(world,this.slingshot)
+        
+}
+
+
+
+fly () {
+this.slingshot.bodyA = null
+
+}
+
+
+display(){
+    if(this.slingshot.bodyA){
+    var pointA = this.slingshot.bodyA.position
+    var pointB = this.pointB
+    line(pointA.x, pointA.y, pointB.x, pointB.y)
+    }
+}
+
+}
+
+
+
+
+
